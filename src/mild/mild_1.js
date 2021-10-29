2465f407b4fe6142775d717fc7ac7814e2ca3a1c
John D. Martin III <john.d.martin.iii@gmail.com>
2021-10-29 12:19:14 -0400

    fix merge

--- src/mild/mild_1.js
+++ src/mild/mild_1.js
@@ -9,11 +9,7 @@
  * see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
  */
 export function sumToString(a, b) {
-<<<<<<< HEAD
     return `${a} + ${b} = ${a + b}`
-=======
-	return `${a} + ${b} = ${a + b}`
->>>>>>> 33fae5841424807e5720881553a8e1deabf16808
 }
 
 
@@ -28,19 +24,11 @@ export function sumToString(a, b) {
  *
  */
 export function getIncreasingArray(startNumber, endNumber) {
-<<<<<<< HEAD
     let array = [];
     for (let i = startNumber; i <= endNumber; i++) {
         array.push(i);
     }
     return array;
-=======
-	let array = [];
-	for (let i = startNumber; i <= endNumber; i++) {
-		array.push(i);
-	}
-	return array;
->>>>>>> 33fae5841424807e5720881553a8e1deabf16808
 }
 
 /**
@@ -51,17 +39,10 @@ export function getIncreasingArray(startNumber, endNumber) {
  * and https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
  */
 export function maxAndMin(numbers) {
-<<<<<<< HEAD
     return {
         max: Math.max(...numbers),
         min: Math.min(...numbers)
     }
-=======
-	return {
-		max: Math.max(...numbers),
-		min: Math.min(...numbers)
-	}
->>>>>>> 33fae5841424807e5720881553a8e1deabf16808
 }
 
 /**
@@ -75,7 +56,6 @@ export function maxAndMin(numbers) {
  *
  */
 export function countArray(array) {
-<<<<<<< HEAD
     let counter = {};
     array.forEach(item => {
         if (counter[item]) {
@@ -85,15 +65,4 @@ export function countArray(array) {
         }
     });
     return counter;
-=======
-	let counter = {};
-	array.forEach(item => {
-		if (counter[item]) {
-			counter[item]++;
-		} else {
-			counter[item] = 1;
-		}
-	});
-	return counter;
->>>>>>> 33fae5841424807e5720881553a8e1deabf16808
 }
